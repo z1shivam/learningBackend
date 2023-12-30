bb1a32e04af1c77cfb8c3c63331ed5d2b45b8844
z1shivam <shivamk.json@gmail.com>
2023-12-31 02:52:37 +0530

    learn utils and added apierror and etc

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// another way to handle this using try catch:
+/*
+const asyncHandlerTC = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+*/
