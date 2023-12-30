fb6e27e059429080e84963d22130c9479194897f
z1shivam <shivamk.json@gmail.com>
2023-12-31 01:56:32 +0530

    learn to connect to mongo db

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\nMonogoDB Connected. DB Host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB Connection Failed: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
